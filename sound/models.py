1d8921187b13d1d18218163cd08b8624ae677671
Shajay-123 <153935689+Shajay-123@users.noreply.github.com>
2024-05-22 16:41:32 +0530

    created at time fetch in the table

--- sound/models.py
+++ sound/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Contact(models.Model):
     STATUS_CHOICES = [
@@ -12,6 +12,7 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=15)
     content = models.TextField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
